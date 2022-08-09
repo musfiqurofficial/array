d8d9290c8d47967f23792732d038a253cfb2cc26
musfiqurofficial <musfiqurofficial1999@gmail.com>
2022-08-09 12:19:59 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,78 @@
+// var friendsAge = [18, 22, 25, 23, 20, 27, 21, 19, 24, 26];
+// var friendsName = [
+//   "John",
+//   "Jane",
+//   "Mary",
+//   "Bob",
+//   "Tom",
+//   "Jack",
+//   "Lily",
+//   "Lucy",
+//   "Peter",
+//   "Linda",
+// ];
+// friendsAge[4] = 208;
+// console.log(friendsAge);
+// // var searchIndex = friendsAge.indexOf(54);
+// // // console.log(searchIndex);
+// // // console.log(friendsAge);
+// // // friendsAge.push(36);
+// // // friendsAge.push(37);
+// // // console.log(friendsAge);
+// // friendsName.push("Mufiqur Rrahman");
+// // // console.log(friendsName);
+// // // console.log(friendsAge);
+// // // friendsAge.unshift(10, 11);
+// // friendsAge.shift();
+// // friendsAge.splice(4, "Musfiqur");
+// // console.log(friendsAge);
+
+
+// var fruts = ["apple", "banana", "orange"];
+// console.log(fruts);
+// var index = fruts.indexOf("banana");
+// console.log(index);
+// var replace = fruts[1] = "mango";
+// console.log(fruts);
+
+// var mark = 20;
+
+// if (mark >= 80) {
+//   console.log("A");
+// }
+// else if (mark >= 60) {
+//   console.log("B");
+// } 
+// else if (mark >= 50) {
+//   console.log("C");
+// }
+// else if (mark >= 40) {
+//   console.log("D");
+// }
+// else {
+//   console.log("F");
+// }
+
+var A = 13;
+var B = 79;
+var C = 45;
+
+if (A > B && A > C) {
+  console.log("A");
+}
+else if (B > A && B > C) {
+  console.log("B");
+}
+else {
+  console.log("C");
+}
+
+a = 9;
+b = 8;
+c = 9;
+if (a == b || a == c || b == c) {
+  console.log("Same");
+}
+else {
+  console.log("Different");
+}
